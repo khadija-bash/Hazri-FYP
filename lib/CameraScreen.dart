4ba07757f4a2ebc0884a512a0ef5f05b4705d97b
khadija-bash <khadija123bashir@gmail.com>
2022-04-23 09:54:50 +0500

    download attendace

--- lib/CameraScreen.dart
+++ lib/CameraScreen.dart
@@ -1,6 +1,5 @@
 import 'dart:io';
 
-import 'package:file_picker/file_picker.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_storage/firebase_storage.dart'  as firebase_storage;
 import 'package:flutter/foundation.dart';
--- lib/DownloadAttendance.dart
+++ lib/DownloadAttendance.dart
@@ -1,5 +1,10 @@
 
+import 'dart:io'; // for File
+import 'package:csv/csv.dart';
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 
 class DownloadAttendance extends StatefulWidget {
   const DownloadAttendance({Key? key}) : super(key: key);
@@ -11,6 +16,48 @@ class DownloadAttendance extends StatefulWidget {
 class _DownloadAttendanceState extends State<DownloadAttendance> {
   List<String> dropdownValuesList = [];
   String dropdownValue = '';
+  String filePath = '';
+  List<String> present = [];
+  var value;
+
+  Future<String> get _localPath async {
+    final directory = await getApplicationDocumentsDirectory();
+
+    return directory.path;
+  }
+
+  Future<File> get _localFile async {
+    final path = await _localPath;
+    return File('$path/attendance.csv');
+  }
+  getCsv() async {
+
+    List<List<dynamic>> rows = <List<dynamic>>[];
+    var collection = FirebaseFirestore.instance.collection('DB-A_Attendance');
+    var docSnapshot = await collection.doc('22-04-2022').get();
+    if (docSnapshot.exists) {
+      Map<String, dynamic>? data = docSnapshot.data();
+      value = data?['Present']; // <-- The value you want to retrieve.
+      // Call setState if needed.
+    }
+
+    rows.add([
+      "Present"
+    ]);
+
+    if (value != null) {
+      for (int i = 0; i < value.length; i++) {
+        List<dynamic> row = <dynamic>[];
+        row.add(value[i]);
+        rows.add(row);
+      }
+
+      File f = await _localFile;
+
+      String csv = const ListToCsvConverter().convert(rows);
+      f.writeAsString(csv);
+    }
+  }
 
   @override
   void initState() {
@@ -60,6 +107,7 @@ class _DownloadAttendanceState extends State<DownloadAttendance> {
                 height: 50,
                 child: ElevatedButton(
                   onPressed: () {
+                    getCsv();
                     /*Download Attendance */
                     Navigator.pop(context);
                   },
